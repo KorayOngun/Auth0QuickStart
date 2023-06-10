c023fd2143cfa5473ac5335856d334e3023ef4da
koray öngün <koraygg28@gmail.com>
2023-06-10 17:07:56 +0300

    Proje dosyası ekle.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleMvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult AuthPage()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
